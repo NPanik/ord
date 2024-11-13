9b666ef30684e549d0ee59cecec39f677f69ebf4
Pavel Nikitin <pavel.nikitin92@gmail.com>
2024-11-13 18:13:51 +0300

    fix env

--- src/options.rs
+++ src/options.rs
@@ -4,7 +4,7 @@ use super::*;
 #[command(group(
   ArgGroup::new("chains")
     .required(false)
-    .args(&["chain_argument", "signet", "regtest", "testnet4"]),
+    .args(&["chain_argument", "signet", "regtest", "testnet"]),
 ))]
 pub struct Options {
   #[arg(long, help = "Load Bitcoin Core data dir from <BITCOIN_DATA_DIR>.")]
